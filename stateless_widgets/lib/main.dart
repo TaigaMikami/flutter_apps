141e78dbb99f50ff4d133cec4f9aebd9f0abfcc9
TaigaMikami <mikamimikami60@gmail.com>
2019-09-13 19:44:28 +0900

    add stateless widgets

---
+++ stateless_widgets/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyApp(),
+    theme: ThemeData(primarySwatch: Colors.green),
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  @over
+}
