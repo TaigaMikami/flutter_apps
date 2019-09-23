cd56df2bc6cb8c2f74c8687f05750858c0e00ddb
TaigaMikami <mikamimikami60@gmail.com>
2019-09-23 23:07:41 +0900

    add admin recipe page

--- recipes_app/lib/login_admin/contentPage.dart
+++ recipes_app/lib/login_admin/contentPage.dart
@@ -1,10 +1,11 @@
 import 'package:recipes_app/widgets/home_page.dart';
 import 'package:recipes_app/pages/myrecipes/list_my_recipe.dart';
-
+import 'package:recipes_app/pages/admin/show_recipe.dart';
 
 abstract class Content {
   Future<HomePageRecipes> lista();
   Future<ListMyRecipe> myrecipe(String id);
+  Future<InicioPage> admin();
 }
 
 class ContentPage implements Content {
@@ -16,5 +17,9 @@ class ContentPage implements Content {
   Future<ListMyRecipe> myrecipe(String id) async {
     print('my list recipe $id');
     return ListMyRecipe(id: id,);
+  }
+
+  Future<InicioPage> admin() async {
+    return InicioPage();
   }
 }
